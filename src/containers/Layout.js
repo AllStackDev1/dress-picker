87a27261c570fb17775e8a4c88434284eb5c6e35
Chinedu Ekene Okpala <allstackdev@gmail.com>
2020-12-03 13:47:00 +0000

    update

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,12 +1,10 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/display-name */
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+// import { useLocation } from 'react-router-dom'
 
 export default ({ children }) => {
-  const { state, pathname } = useLocation()
+  // const { state, pathname } = useLocation()
 
-  return (
-    <div className='site-layout-content'>{children}</div>
-  )
+  return <div className='site-layout-content'>{children}</div>
 }
